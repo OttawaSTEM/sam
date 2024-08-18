e8d76924fd70936d50db3be743a5ec1d40fc472b
OttawaSTEM <ottawastem@gmail.com>
2024-08-18 11:30:26 -0400

    update

--- lib/pages/scan/ui/scan.dart
+++ lib/pages/scan/ui/scan.dart
@@ -79,9 +79,9 @@ class _ScanPageState extends State<ScanPage> {
     device.connectAndUpdateStream().catchError((e) {
       Snackbar.show(ABC.c, prettyException("Connect Error:", e), success: false);
     });
-    MaterialPageRoute route = MaterialPageRoute(builder: (context) => DeviceScreen(device: device), settings: RouteSettings(name: '/DeviceScreen'));
+    MaterialPageRoute route = MaterialPageRoute(builder: (context) => DeviceScreen(device: device), settings: const RouteSettings(name: '/DeviceScreen'));
     Navigator.of(context).push(route);
-    // Get.toNamed('/control/');
+    Get.toNamed('/control/');
   }
 
   Future onRefresh() {
